cfcb0ecd1736e30fb861bcfb7e045feac85bd176
Yassirben97 <cookconnekt@gmail.com>
2025-06-12 00:58:46 +0100

    Update App.tsx

--- project-bolt-sb1-yh5bcbmr (1) n/project/src/App.tsx
+++ project-bolt-sb1-yh5bcbmr (1) n/project/src/App.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import Header from './components/Header';
 import CooksPage from './components/CooksPage';
 import TendersPage from './components/TendersPage';
